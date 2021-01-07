e90f3dcf2c7d5cafbd58eb22655b2b03c318cbab
Maciej <neipreis22@gmail.com>
2021-01-07 17:56:52 +0100

    created add printer page, also printer page now generate block based on data from database

--- Printy/Printy.Web/Controllers/HomeController.cs
+++ Printy/Printy.Web/Controllers/HomeController.cs
@@ -19,10 +19,6 @@ namespace Printy.Web.Controllers {
             return View();
         }
 
-        public IActionResult Privacy() {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- Printy/Printy.Web/Controllers/PrintersController.cs
+++ Printy/Printy.Web/Controllers/PrintersController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Printy.Web.Database;
+using Printy.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +8,28 @@ using System.Threading.Tasks;
 
 namespace Printy.Web.Controllers {
     public class PrintersController : Controller {
+        private readonly PrintyDbContext _dbContext;
+        public PrintersController(PrintyDbContext dbContext) {
+            _dbContext = dbContext;
+        }
         public IActionResult Index() {
+            var printers = _dbContext.Printers.ToList();
+            ViewData["Printers"] = printers;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddPrinter() {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddPrinter(Printer printer) {
+            await _dbContext.Printers.AddAsync(printer);
+            _dbContext.SaveChanges();
+
+            return LocalRedirect("/Printers/Index");
+        }
+
+        public IActionResult PrinterDetails(Printer printer) {
             return View();
         }
     }
