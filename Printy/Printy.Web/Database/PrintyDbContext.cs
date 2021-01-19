a09040036043ee321a1148faf1cc64ca3a504752
Maciej <neipreis22@gmail.com>
2021-01-19 12:29:22 +0100

    added migration example data to ModelBuilder

--- Printy/Printy.Web/Database/PrintyDbContext.cs
+++ Printy/Printy.Web/Database/PrintyDbContext.cs
@@ -9,11 +9,49 @@ namespace Printy.Web.Database {
     public class PrintyDbContext : DbContext {
         public PrintyDbContext(DbContextOptions options) : base(options) {}
 
+        #region DbSets
+
         public DbSet<Printer> Printers { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserOrder> Orders { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder) {}
+        #endregion
 
+        protected override void OnModelCreating(ModelBuilder builder) {
+            InitPrinters(builder);
+            InitUsers(builder);
+            InitOrders(builder);
         }
+
+        #region InitMethods
+
+        private static void InitPrinters(ModelBuilder modelBuilder) => modelBuilder.Entity<Printer>().HasData(
+            new { Id = 1, Name = "First Printer", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer1@test.com", PhoneNumber = "123456789" },
+                    new { Id = 2, Name = "Second Printer", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer2@test.com", PhoneNumber = "123456789" },
+                    new { Id = 3, Name = "Printer 3", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer3@test.com", PhoneNumber = "123456789" },
+                    new { Id = 4, Name = "Printer Printer", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer4@test.com", PhoneNumber = "123456789" },
+                    new { Id = 5, Name = "Piąta drukarka", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer5@test.com", PhoneNumber = "123456789" },
+                    new { Id = 6, Name = "Printer 6", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer6@test.com", PhoneNumber = "123456789" },
+                    new { Id = 7, Name = "Printer 7", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer7@test.com", PhoneNumber = "123456789" },
+                    new { Id = 8, Name = "Printer 8", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer8@test.com", PhoneNumber = "852753951" },
+                    new { Id = 9, Name = "Printer no. 9", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer9@test.com", PhoneNumber = "025846759" },
+                    new { Id = 10, Name = "Test Printer", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer10@test.com", PhoneNumber = "005789111" },
+                    new { Id = 11, Name = "Printer #11", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer11@test.com", PhoneNumber = "070030012" },
+                    new { Id = 12, Name = "Printero", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer12@test.com", PhoneNumber = "007114500" },
+                    new { Id = 13, Name = "Print by this", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", Email = "printer13@test.com", PhoneNumber = "224444444" }
+            );
+
+        private static void InitUsers(ModelBuilder modelBuilder) => modelBuilder.Entity<User>().HasData(
+                new { Id = 1, Name = "Marek", Surname = "Wiertarka", Email = "marek.wiertarka@test.com", Password = "Test123@" },
+                new { Id = 2, Name = "Basia", Surname = "Kania", Email = "printer1@test.com", Password = "Test123@" },
+                new { Id = 3, Name = "Wojtek", Surname = "Kapeć", Email = "wkapek@test.com", Password = "Test123@" }
+            );
+
+        private static void InitOrders(ModelBuilder modelBuilder) => modelBuilder.Entity<UserOrder>().HasData(
+                new { Id = 1, UserId = 2, PrinterId = 2, UserFile = "Systemy zarządzania zadaniami w projekcie", FileCopies = 13 },
+                new { Id = 2, UserId = 2, PrinterId = 8, UserFile = "MojeNotatki", FileCopies = 8 }
+            );
+
+        #endregion
+    }
 }
---
+++ Printy/Printy.Web/Migrations/20210119112730_DatabaseData.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Printy.Web.Migrations
+{
+    public partial class DatabaseData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "FileCopies", "PrinterId", "UserFile", "UserId" },
+                values: new object[,]
+                {
+                    { 2, 8, 8, "MojeNotatki", 2 },
+                    { 1, 13, 2, "Systemy zarządzania zadaniami w projekcie", 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Printers",
+                columns: new[] { "Id", "Description", "Email", "Name", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 13, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer13@test.com", "Print by this", "224444444" },
+                    { 12, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer12@test.com", "Printero", "007114500" },
+                    { 11, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer11@test.com", "Printer #11", "070030012" },
+                    { 10, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer10@test.com", "Test Printer", "005789111" },
+                    { 8, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer8@test.com", "Printer 8", "852753951" },
+                    { 9, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer9@test.com", "Printer no. 9", "025846759" },
+                    { 6, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer6@test.com", "Printer 6", "123456789" },
+                    { 5, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer5@test.com", "Piąta drukarka", "123456789" },
+                    { 4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer4@test.com", "Printer Printer", "123456789" },
+                    { 3, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer3@test.com", "Printer 3", "123456789" },
+                    { 2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer2@test.com", "Second Printer", "123456789" },
+                    { 7, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer7@test.com", "Printer 7", "123456789" },
+                    { 1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo nisi at sapien luctus, a ornare est consequat.", "printer1@test.com", "First Printer", "123456789" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name", "Password", "Surname" },
+                values: new object[,]
+                {
+                    { 1, "marek.wiertarka@test.com", "Marek", "Test123@", "Wiertarka" },
+                    { 2, "printer1@test.com", "Basia", "Test123@", "Kania" },
+                    { 3, "wkapek@test.com", "Wojtek", "Test123@", "Kapeć" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Printers",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
